760fd1e5c16529d1700316c57784a9b99edd236f
Isaac Graciano <isaacgraciano123@gmail.com>
2022-06-02 17:42:04 -0300

    Planos

--- src/Components/PlanoEscolhido.js
+++ src/Components/PlanoEscolhido.js
@@ -1,4 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
+import styled from 'styled-components';
+
+
 export default function PlanoEscolhido() {
+    const {ID_DO_PLANO} = useParams();
+    
     return (
         <>
         </>
--- src/Components/Planos.js
+++ src/Components/Planos.js
@@ -1,6 +1,86 @@
+import React, { useEffect, useState, useContext } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { BaseAPI } from "../Global Data/Data";
+import UserContext from "../Context/UserContext";
+
 export default function Planos() {
+    const [plano, setPlano] = useState([]);
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getPlanos()
+    }, [plano])
+
+    function getPlanos() {
+        const config = {
+            headers: {
+                "Authorization": `Bearer ${user.token}`
+            }
+        }
+
+        const promise = axios.get(`${BaseAPI}/subscriptions/memberships`, config)
+        promise.then(response => setPlano(response.data))
+
+    }
+
     return (
-        <>
-        </>
+        <Container>
+            <p>Escolha seu Plano</p>
+            <ListarPlanos>
+                {plano.map(plano => 
+                <Plano>
+                    <Link to = {`/subscription/${plano.id}`}>
+                        <img src = {plano.image} alt = {plano.id}/>
+                        <span>R$ {plano.price}</span>
+                    </Link>
+                </Plano>
+                    )}
+            </ListarPlanos>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: #0E0E13;
+    p{
+        font-size: 32px;
+        color: #FFFFFF;
+        margin-top: 29px;
+    }
+`
+
+const ListarPlanos = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 24px;
+`
+const Plano = styled.div`
+    display: flex;
+    align-items: center;
+    height: 180px;
+    width: 350px;
+    margin-bottom: 10px;
+    position: relative;
+    border: 3px solid #7E7E7E ;
+    border-radius: 12px;
+    span{
+        margin-left: 30px;
+        margin-top: 45px;
+        position: absolute;
+        font-size: 24px;
+        color: #FFFFFF;
+    }
+    img{
+        margin-left: 30px;
+        height: 95px;
+        width: 140px;
+    }
+`
