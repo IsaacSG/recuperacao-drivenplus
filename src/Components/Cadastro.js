8faf5f18a759d8f1a44d2477833c50fe697a6f9c
Isaac Graciano <54876928+IsaacSG@users.noreply.github.com>
2022-06-06 15:03:58 -0300

    Update Cadastro.js

--- src/Components/Cadastro.js
+++ src/Components/Cadastro.js
@@ -49,7 +49,7 @@ const Container = styled.div`
     justify-content: center;
     background-color: #0E0E13;
     padding-top: 147px;
-    padding-bottom: 50px;
+    padding-bottom: 150px;
 
     input{
         height: 52px;
